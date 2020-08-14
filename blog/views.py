3052bc5c9d0511a8ab5aa3abdc7f008eb188f003
machariamwangi <elijahmacharia54@gmail.com>
2020-08-14 14:43:27 +0300

    templating in django

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+posts = [
+    {
+        'author': 'elijah',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'AUGUST 14 2020',
+    },
+    {
+        'author': 'jOHN DOE',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'AUGUST 15 2020',
+    }
+
+]
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
